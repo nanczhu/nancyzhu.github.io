9156565a48cd17bac24354e3b41a8f8c9ea65708
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-15 22:22:16 -0800

    Update colorSchemes.js

--- colorSchemes.js
+++ colorSchemes.js
@@ -1,10 +1,11 @@
-function color() {
-if(localStorage.getItem("color-mode", "dark"))
-{
-     setItem("color-mode", "dark");
-     let DarkMode = getItem("color-mode");
- }
- else {
-     window.localStorage.setItem("color-mode", "dark");
-     let DarkMode = window.localStorage.getItem("color-mode");
- }
+  function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+  }
+  function color() {
+    if (localStorage.getItem("color-mode")) {
+      colorMode = localStorage.getItem("color-mode");
+    }
+    else {
+      let rdColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+      document.body.style.backgroundColor = localStorage.getItem(rdColor);
+    }
